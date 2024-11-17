2ac48cda608951645ef7c80512980998904da60e
Binaya Ginesh <144934109+binayaginesh@users.noreply.github.com>
2024-11-18 00:25:00 +0530

    Create armstrong.java

---
+++ package/armstrong.java
@@ -0,0 +1,26 @@
+package Armstrong;
+
+import java.io.*;
+import java.util.*;
+
+ public class armstrong{
+  public void find(){
+    int n,sum=0,cube,i;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a 3 digit number");
+    n = sc.nextInt();
+    int newn=n;
+    for(i=0;i<n;i++){
+       int rem = n%10;
+       cube = rem*rem*rem;
+       sum=sum+cube;
+       n=n/10;  
+    }
+    if(newn==sum){
+       System.out.println("it is a armstrong number");
+    }
+    else{
+       System.out.println("it is not armstrong");
+    }
+  }
+}
